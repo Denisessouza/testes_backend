66e9ae20b4a64e7bd86118410a81c25097ef195b
Denise Souza <95714393+Denisessouza@users.noreply.github.com>
2022-11-02 19:02:59 -0300

    Teste Unitário - ER2

---
+++ Projeto_teste/Projeto_teste/Operacoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_teste
+{
+   public static class Operacoes //Static é para não precisar instanciar para ser chamada
+    {
+        public static double Somar(double pNum, double sNum)
+        {
+            return (pNum + sNum);
+        }
+
+        
+    }
+}
